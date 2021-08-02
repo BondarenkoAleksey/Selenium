f750840e3007080fc84171fe5eac95d82c3ef5f3
relaxmanz <bonddd@gmail.com>
2021-08-02 17:51:20 +0300

    comments

--- selenium_buy_a_distiller_in_dns-shop.ru.py
+++ selenium_buy_a_distiller_in_dns-shop.ru.py
@@ -10,6 +10,7 @@ try:
     random = random.randint(1, 3)
     phone_number = "9012345678"
 
+    #переход по категориям товаров
     appliances = browser.find_element_by_css_selector("div:nth-child(1) > div.menu-desktop__root-info > a")
     appliances.click()
 
@@ -30,6 +31,7 @@ try:
 
     time.sleep(random)
 
+    #кнопка покупки
     buy_button = browser.find_element_by_css_selector("body > div.container.category-child > div > div.products-page__content > div.products-page__list > div.products-list > div > div.catalog-products.view-simple > div:nth-child(2) > div.product-buy.product-buy_one-line.catalog-product__buy > button.button-ui.buy-btn.button-ui_brand")
     buy_button.click()
 
@@ -40,30 +42,35 @@ try:
 
     time.sleep(random)
 
+    #переход в корзину
     place_an_order = browser.find_element_by_xpath('//*[@id="total-amount"]/div/div[2]/button')
     place_an_order.click()
 
     time.sleep(random)
 
+    #ввод номера телефона
     phone = browser.find_element(By.XPATH, "/html/body/div/div/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div/div/input")
-    phone.send_keys("9012345678")
+    phone.send_keys(phone_number)
 
     time.sleep(random)
 
+    #оплата при получении
     upon_receipt = browser.find_element_by_xpath("/html/body/div/div/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[1]")
     upon_receipt.click()
 
     time.sleep(random)
 
+    #отказ от регистрации в программе ProZaPas
     uncheck = browser.find_element_by_xpath('//div[1]/label[1]/span[1]')
     uncheck.click()
 
     time.sleep(random + 1)
 
+    #подтверждение заказа
     confirm_the_order = browser.find_element_by_css_selector("div.checkout-container__apply-wrapper > div > div")
     confirm_the_order.click()
 
-    time.sleep(random)
+    time.sleep(random - 1)
 
 
 finally:
