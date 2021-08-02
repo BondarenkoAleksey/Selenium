d3793e651dccd993d6e2916da1e3b80cb47bf338
relaxmanz <bonddd@gmail.com>
2021-08-02 17:35:10 +0300

    Buying a distiller in an online store dns-shop.ru

---
+++ selenium_buy_a_distiller_in_dns-shop.ru.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import random
+
+try:
+    link = "https://www.dns-shop.ru/"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    random = random.randint(1, 3)
+    phone_number = "9012345678"
+
+    appliances = browser.find_element_by_css_selector("div:nth-child(1) > div.menu-desktop__root-info > a")
+    appliances.click()
+
+    time.sleep(random)
+
+    kitchen_appliances = browser.find_element_by_xpath("//div[1]/a[1]/div[1]/span")
+    kitchen_appliances.click()
+
+    time.sleep(random)
+
+    preparation_of_drinks = browser.find_element(By.CSS_SELECTOR, "a:nth-child(3) > div.subcategory__content > div > picture > img")
+    preparation_of_drinks.click()
+
+    time.sleep(random)
+
+    distillers = browser.find_element(By.XPATH, "//a[12]/div[1]/div/picture/img")
+    distillers.click()
+
+    time.sleep(random)
+
+    buy_button = browser.find_element_by_css_selector("body > div.container.category-child > div > div.products-page__content > div.products-page__list > div.products-list > div > div.catalog-products.view-simple > div:nth-child(2) > div.product-buy.product-buy_one-line.catalog-product__buy > button.button-ui.buy-btn.button-ui_brand")
+    buy_button.click()
+
+    time.sleep(random)
+
+    shopping_cart = browser.find_element(By.XPATH, "//div[2]/a[3]/span[3]/span[1]")
+    shopping_cart.click()
+
+    time.sleep(random)
+
+    place_an_order = browser.find_element_by_xpath('//*[@id="total-amount"]/div/div[2]/button')
+    place_an_order.click()
+
+    time.sleep(random)
+
+    phone = browser.find_element(By.XPATH, "/html/body/div/div/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div/div/input")
+    phone.send_keys("9012345678")
+
+    time.sleep(random)
+
+    upon_receipt = browser.find_element_by_xpath("/html/body/div/div/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[1]")
+    upon_receipt.click()
+
+    time.sleep(random)
+
+    uncheck = browser.find_element_by_xpath('//div[1]/label[1]/span[1]')
+    uncheck.click()
+
+    time.sleep(random + 1)
+
+    confirm_the_order = browser.find_element_by_css_selector("div.checkout-container__apply-wrapper > div > div")
+    confirm_the_order.click()
+
+    time.sleep(random)
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
